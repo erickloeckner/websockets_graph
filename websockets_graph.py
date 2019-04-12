e1e1ce2e19b9f31c003fc0530bd670dd0e5a7adc
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2019-04-12 10:10:51 -0400

    Added proper shutdown to main loop

--- websockets_graph.py
+++ websockets_graph.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3.6
 
 import asyncio
 import datetime
 import json
 import os
 import random
+import signal
 import websockets
 from collections import deque
 
 NUM_POINTS = 100
-TOTAL_W = 780
-TOTAL_H = 380
+TOTAL_W = 1260
+TOTAL_H = 700
 MARGIN = 10
 
 x_spacing = TOTAL_W / NUM_POINTS
@@ -18,7 +19,7 @@ x_vals = [i * x_spacing + (x_spacing / 2) + MARGIN for i in range(NUM_POINTS)]
 
 y_vals = deque(maxlen = NUM_POINTS)
 for i in range(NUM_POINTS):
-    y_vals.append(0)
+    y_vals.append(TOTAL_H)
     
 #~ vals_combined = []
 vals_out = ""
@@ -27,10 +28,10 @@ async def ws_send(websocket, path):
     try:
         while True:        
             load_avg = os.getloadavg()[0] / 4
-            if load_avg > 4:
-                load_avg = 4
+            if load_avg > 1:
+                load_avg = 1
             vals_combined = []
-            y_vals.append(380 - (load_avg * 380) + MARGIN)
+            y_vals.append(TOTAL_H - (load_avg * TOTAL_H) + MARGIN)
             
             for i in zip(x_vals, y_vals):
                 vals_combined.append(str(i[0]) + "," + str(i[1]))
@@ -42,7 +43,20 @@ async def ws_send(websocket, path):
     except websockets.exceptions.ConnectionClosed:
         pass
 
-start_server = websockets.serve(ws_send, '127.0.0.1', 5678)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def sock_server(stop):
+    print("Starting server...")
+    async with websockets.serve(ws_send, '127.0.0.1', 5678):
+        await stop
+        print("\nShutting down server")
+#~ start_server = websockets.serve(ws_send, '127.0.0.1', 5678)
+
+#~ asyncio.get_event_loop().run_until_complete(start_server)
+#~ asyncio.get_event_loop().run_forever()
+
+loop = asyncio.get_event_loop()
+stop = loop.create_future()
+loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+
+loop.run_until_complete(sock_server(stop))
